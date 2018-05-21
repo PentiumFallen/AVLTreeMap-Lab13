854c13cf9b4f3a0415c432bf5933d0d86a81ddd5
PentiumFallen <raul.vargas@upr.edu>
2018-05-21 10:23:30 -0400

    Only commit

---
+++ src/testerClasses/RunMe.java
@@ -0,0 +1,15 @@
+package testerClasses;
+
+public class RunMe {
+
+	public static void main(String[] args) {
+		System.out.println("Goodbye!");
+		System.out.println(" [ ]");
+		System.out.println("\\ | /");
+		System.out.println(" \\|/");
+		System.out.println( "  |");
+		System.out.println( "  |");
+		System.out.println( " / \\");
+		System.out.println( "/   \\");
+	}
+}
